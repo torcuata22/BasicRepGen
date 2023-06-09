351bf8415b2f77ef0cbf46918503a3d4ac64c355
torcuata22 <marilynjmarquez@gmail.com>
2023-06-09 12:43:13 -0400

    five

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    rent = models.IntegerField()
+    emi = models.IntegerField()
+    tax = models.IntegerField()
+    exp = models.IntegerField()
+
+
+
