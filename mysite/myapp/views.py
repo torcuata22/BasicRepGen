9a9252b049e38102a4ca8d5a592ef0d5e4df8747
torcuata22 <marilynjmarquez@gmail.com>
2023-06-09 12:33:48 -0400

    fourth

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd 
+from pandas.io import json
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('Hello there')
+    if request.method == 'POST':
+        myfile=request.FILES['file']
+        df=pd.read_csv(myfile)
+        json_records=df.reset_index().to_json(orient='records')
+        data=[]
+        #create list of properties
+        data = json.loads(json_records)
+        print(data)
+       
+    print("this is a GET request")
+    return render(request, 'myapp/index.html')
